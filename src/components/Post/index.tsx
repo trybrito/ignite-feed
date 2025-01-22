2691ef1ce8886f62815ea016411a705fba23564c
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-01-22 17:26:34 -0300

    feat: showing posts

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,36 +1,61 @@
 import { Avatar } from "../Avatar";
 import { Comment } from "../Comment";
+
 import styles from "./styles.module.css";
 
-export function Post() {
+import { format, formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+type Props = {
+  author: {
+    avatarUrl: string;
+    name: string;
+    role: string;
+  };
+  content: {
+    type: string;
+    content: string;
+  }[];
+  publishedAt: Date;
+};
+
+export function Post({ author, content, publishedAt }: Props) {
+  const publishedDateTitle = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar size="lg" src="https://github.com/diego3g.png" />
+          <Avatar size="lg" src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Diego Fernandes</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="19 de Janeiro às 20:20" dateTime="2025-01-19 20:20:00">
-          Publicado há 1h
+        <time title={publishedDateTitle} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
         </time>
       </header>
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          👉 <a href="#">jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="#">#novoprojeto</a>&nbsp;
-          <a href="#">#nlw</a>&nbsp;
-          <a href="#">#rocketseat</a>&nbsp;
-        </p>
+        {content.map((line) => {
+          switch (line.type) {
+            case "paragraph":
+              return <p>{line.content}</p>;
+            case "link":
+              return (
+                <p>
+                  <a href="#">{line.content}</a>
+                </p>
+              );
+          }
+        })}
       </div>
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
@@ -40,11 +65,7 @@ export function Post() {
         </footer>
       </form>
 
-      <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
-      </div>
+      <div className={styles.commentList}></div>
     </article>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,51 @@ import { Sidebar } from "../../components/Sidebar";
 import { Post } from "../../components/Post";
 import styles from "./styles.module.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2025-01-02 14:32:15"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2025-10-02 21:10:25"),
+  },
+];
+
 export function Home() {
   return (
     <div>
@@ -11,9 +56,16 @@ export function Home() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
-          <Post />
+          {posts.map(({ author, content, publishedAt }, index) => {
+            return (
+              <Post
+                author={author}
+                content={content}
+                publishedAt={publishedAt}
+                key={index}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
